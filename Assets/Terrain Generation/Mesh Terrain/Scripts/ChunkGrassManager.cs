91ea03854355c15964ae66f0bdd6b081cf44fff7
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2023-02-12 12:34:22 -0700

    Grass improvements
    + Random grass rotation
    + Minimum grass height
    + Better mesh terrain textures

--- Assets/Terrain Generation/Mesh Terrain/Scripts/ChunkGrassManager.cs
+++ Assets/Terrain Generation/Mesh Terrain/Scripts/ChunkGrassManager.cs
@@ -1,21 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ChunkGrassManager : MonoBehaviour
 {
-    [SerializeField] private GameObject patch;
+
+    [SerializeField] private float minGrassHeight;
+    // [SerializeField] private GameObject patch;
     [SerializeField] private GameObject[] grass;
+    // [SerializeField] private Transform grassHolder;
+    
+    /*private void Start()
+    {
+        grass = new GameObject[64 * 64];
+        for (int x = 0; x < 64; x++)
+        {
+            for (int z = 0; z < 64; z++)
+            {
+                grass[x * 64 + z] = Instantiate(patch, new Vector3(x, 10, z), new Quaternion(0, 0, 0, 0), transform);
+            }
+        }
+    }*/
 
     public void PlaceGrass(float[,] heightMap, float maxHeight)
     {
         for (int i = 0; i < grass.Length; i++)
         {
-            // Vector3 position = grass[i].transform.position;
-            var x = Mathf.FloorToInt(i / 32f);
-            var z = i % 32;
-            Debug.Log("i: " + i + " x: " + x + " z: " + z);
-            grass[i].transform.position = new Vector3(x, heightMap[x, z] * maxHeight, z);
+            var x = Mathf.FloorToInt(i / 64f);
+            var z = i % 64;
+            var height = heightMap[x, z] * maxHeight;
+            
+            if (height < minGrassHeight)
+            {
+                grass[i].SetActive(false);
+            }
+            else
+            {
+                grass[i].transform.position = new Vector3(transform.position.x + x, height, transform.position.z + z);
+                grass[i].transform.rotation = Quaternion.Euler(0,Random.Range(0,360),0);
+            }
         }
     }
 }
